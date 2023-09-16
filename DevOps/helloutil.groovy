6cd139072e00bd66e1c25e47fed63d73e86e42b7
yang.yang <jpuyy.com@gmail.com>
2023-09-16 12:19:16 +0800

    Add scm pipeline for jenkins

---
+++ DevOps/helloutil.groovy
@@ -0,0 +1,12 @@
+def createLunch(Map params = [:]) {
+    def lunch = params.get("lunch")
+    def price = params.get("price")
+    def order = """[
+        {
+            "price": ${price},
+            "lunch": ${lunch}
+        }
+    ]"""
+
+    writeJSON file: ".tmp/order.json", json: "${order}"
+}
