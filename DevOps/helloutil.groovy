887247810caaa298bb642baf41b4c79e999c93e3
yang.yang <jpuyy.com@gmail.com>
2023-09-16 18:18:10 +0800

    Test switch case

--- DevOps/helloutil.groovy
+++ DevOps/helloutil.groovy
@@ -1,12 +1,27 @@
 def createLunch(Map params = [:]) {
     def lunch = params.get("lunch")
     def price = params.get("price")
-    def order = """[
-        {
-            "price": ${price},
-            "lunch": ${lunch}
-        }
-    ]"""
+
+    String drinkType = params.get('drink', null)
+
+    switch(drinkType) {
+        case "cococola":
+            def order = """[
+                {
+                    "price": ${price},
+                    "lunch": ${lunch},
+                    "drink": ${drinkType}
+
+                }
+            ]"""
+        default:
+            def order = """[
+                {
+                    "price": ${price},
+                    "lunch": ${lunch}
+                }
+            ]"""
+    }
 
     echo "This is your order:"
     echo "${order}"
